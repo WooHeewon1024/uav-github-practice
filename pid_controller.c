5fc934da2167e50214ec8e1dd6f8d562a5d0cd63
WooHeewon <jacky_1024@naver.com>
2025-12-30 15:01:12 +0900

    code update

--- pid_controller.c
+++ pid_controller.c
@@ -2,3 +2,12 @@
 // TODO: implement PID control logic
 // PID turning experiment
 // VS Code Git test
+
+#include <stdio.h>
+
+int main(){
+    int x;
+    scanf("%d",&x);
+    printf("%d",x);
+    return 0;
+}
